0d2f1d2b3c854634380b70b0e67ec8abec09f9ed
JayCuevas <Jordan.Cuevas@precisecal.com>
2020-12-28 20:29:34 -0500

    Refactored code Created functions out of existing blocks Moved static strings into variables

--- newegg.py
+++ newegg.py
@@ -13,85 +13,126 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
-client = Client("Twilioemail", "Twilio password")# add info here for twilio account
+# Twilio credentials
+
+# client = Client(twilio_email, twilio_password)
 
 
 class NeweggSpider(scrapy.Spider):
     name = "newegg"
+
+    newegg_password = ""
+    twilio_email = ""
+    twilio_password = ""
+
+    firefox_profile_path = "/home/jaydlc/.mozilla/firefox/4unhxga4.default-release"
+
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) " \
                  "Chrome/43.0.2357.130 Safari/537.36 "
 
+    # product_url = "https://www.newegg.com/p/pl?d=3080&N=100007709%204841%2050001314%204021%2050001315%2050001312%2050001402&isdeptsrh=1"
+    product_url = 'https://www.newegg.com/adata-model-auv350-32g-rbk-32gb/p/N82E16820215361?Description=usb&cm_re=usb-_-20-215-361-_-Product'
+
     def __init__(self, *args, **kwargs):
         options = Options()
-        options.headless = True
-        self.profile = webdriver.FirefoxProfile(
-            r'mozilla profile') #mozilla profile
-        self.driver = webdriver.Firefox(self.profile, options=options, executable_path=GeckoDriverManager().install())
+        options.headless = False
+
+        self.profile = webdriver.FirefoxProfile(self.firefox_profile_path)
+        self.driver = webdriver.Firefox(
+            self.profile, options=options, executable_path=GeckoDriverManager().install())
         self.wait = WebDriverWait(self.driver, 5)
         self.products = []
-        self.start_urls = ["https://www.newegg.com/p/pl?d=3080&N=100007709%204841%2050001314%204021%2050001315%2050001312%2050001402&isdeptsrh=1", ]# Link for product, this is 3080 with some presets
+        # Link for product, this is 3080 with some presets
+        self.start_urls = [self.product_url]
+
+    def add_to_cart(self):
+        print("\nFound 1 item to add to cart.\n")
+        add_to_cart_xpath = "//*[@class='btn btn-primary btn-wide']"
+
+        # Wait until we can click on the add to cart button
+        self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, add_to_cart_xpath)))
+        time.sleep(.5)
+        self.driver.find_element_by_xpath(
+            add_to_cart_xpath).click()
+
+    # def move_to_checkout(self):
+    #     print("\nGoing to Checkout Cart. (Page 2)\n")
+    #     self.driver.get(
+    #         "https://secure.newegg.com/Shopping/ShoppingCart.aspx?Submit=view")  
+    #     try:
+    #         print("\nClicking Secure Checkout. (Page 2)\n")
+    #         available = self.driver.find_element_by_xpath(
+    #             "//*[@class='btn btn-primary btn-wide']").is_enabled()
+    #         if available:
+    #             time.sleep(1)
+    #             self.driver.find_element_by_xpath(
+    #                 "//*[@class='btn btn-primary btn-wide']").click()
+    #             return True
+    #     except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
+    #         if error:
+    #             print("\nSecuring Checkout button is not Clickable.\n")
+    #             print(error)
+    #             time.sleep(3)
+    #             yield Request(self.product_url, callback=self.parse, dont_filter=True)
+
+    def get_products(self):
+        self.products = self.driver.find_element_by_xpath(
+            "//*[@class='btn btn-primary btn-mini']").text.strip()
+
+    def product_available(self):
+        return len(self.products)
 
     def parse(self, response, *args, **kwargs):
-        url = 'https://www.newegg.com/p/pl?d=3080&N=100007709%204841%2050001314%204021%2050001315%2050001312%2050001402&isdeptsrh=1'#same link again
-        self.driver.get(url)
+        self.driver.get(self.product_url)
         # Finding Product Availability.
         try:
-            self.products = []
-            self.products = self.driver.find_element_by_xpath("//*[@class='btn btn-primary btn-mini']").text.strip()
-            print("\nProduct is Available.\n")
-            if len(self.products):
-                for product in self.products:
-                    # Starting Bot. (Page 1)
-                    print("\nFound 1 item to add to cart.\n")
-                    self.wait.until(
-                        EC.visibility_of_element_located((By.XPATH, "//*[@class='btn btn-primary btn-mini']")))
-                    time.sleep(.5)
-                    self.driver.find_element_by_xpath("//*[@class='btn btn-primary btn-mini']").click()
-                    time.sleep(.5)
-                    break
+            self.get_products()
+
+            if self.product_available():
+                print("\nProduct is Available.\n")
+                self.add_to_cart()
         except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
             if error:
                 print("\nProduct Sold Out: Retrying in 3 Seconds.\n")
                 time.sleep(3)
-                yield Request(url, callback=self.parse, dont_filter=True)
+                yield Request(self.product_url, callback=self.parse, dont_filter=True)
 
-        if len(self.products):
+        if self.product_available():
             # Going to Check Out Page. (Page 2)
+            # self.move_to_checkout()
+
             print("\nGoing to Checkout Cart. (Page 2)\n")
-            self.driver.get("https://secure.newegg.com/Shopping/ShoppingCart.aspx?Submit=view")#cart
+            self.driver.get(
+                "https://secure.newegg.com/Shopping/ShoppingCart.aspx?Submit=view")  
             try:
                 print("\nClicking Secure Checkout. (Page 2)\n")
-                available = self.driver.find_element_by_xpath("//*[@class='btn btn-primary btn-wide']").is_enabled()
+                available = self.driver.find_element_by_xpath(
+                    "//*[@class='btn btn-primary btn-wide']").is_enabled()
                 if available:
                     time.sleep(1)
-                    self.driver.find_element_by_xpath("//*[@class='btn btn-primary btn-wide']").click()
+                    self.driver.find_element_by_xpath(
+                        "//*[@class='btn btn-primary btn-wide']").click()
+                    return True
             except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
                 if error:
                     print("\nSecuring Checkout button is not Clickable.\n")
+                    print(error)
                     time.sleep(3)
-                    yield Request(url, callback=self.parse, dont_filter=True)
-
+                    yield Request(self.product_url, callback=self.parse, dont_filter=True)
             # Login Password. (Page 3)
             # try:
             #     print("\nAttempting Password. (Page 3)\n")
-            #     self.wait.until(EC.visibility_of_element_located((By.ID, "labeled-input-password")))
-            #     password = self.driver.find_element_by_id("labeled-input-password")
-            #     password.send_keys("###") # NEWEGG PASSWORD HERE
+            #     self.wait.until(EC.visibility_of_element_located(
+            #         (By.ID, "labeled-input-password")))
+            #     password = self.driver.find_element_by_id(
+            #         "labeled-input-password")
+            #     password.send_keys(self.newegg_password)
             #     password.send_keys(Keys.ENTER)
             # except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
             #     if error:
             #         print("\nDid Not Use Password. (Page 3)\n")
-            try:
-                print("\nMoving on with payment (Page 3)\n")
-                test123 = self.driver.find_element_by_xpath("//*[@class='btn btn-primary checkout-step-action-done layout-quarter']").is_enabled()
-                if test123:
-                    time.sleep(1)
-                    self.driver.find_element_by_xpath("//*[@class='btn btn-primary checkout-step-action-done layout-quarter']").click()
-            except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
-                if error:
-                    print("\nNot WORKING\n")
-                    time.sleep(3)
-                    yield Request(url, callback=self.parse, dont_filter=True)
+
             # Submit CVV Code(Must type CVV number. (Page 4)
             try:
                 print("\nTrying Credit Card CVV Number. (Page 4)\n")
@@ -101,29 +142,28 @@ class NeweggSpider(scrapy.Spider):
                     "//input[@class='form-text mask-cvv-4'][@type='text']")
                 time.sleep(1)
                 security_code.send_keys(
-                    Keys.BACK_SPACE + Keys.BACK_SPACE + Keys.BACK_SPACE + "###")  # You can enter your CVV number here.!!!!!!!!!
+                    Keys.BACK_SPACE + Keys.BACK_SPACE + Keys.BACK_SPACE + "123")  # You can enter your CVV number here.!!!!!!!!!
             except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
                 if error:
                     print("\nCould Not Type CVV. (Page 4)\n")
 
             # ARE YOU READY TO BUY? (Page 4) COMMENT BELOW OUT IF YOU DONT WANT IT TO BUY STUFF
 
-
-            try:
-                print("\nBuying Product. (Page 4)\n")
-                time.sleep(.5)
-                final_step = self.driver.find_element_by_id("btnCreditCard").is_enabled()
-                if final_step:
-                    print("\nFinalizing Bot!...\n")
-                    self.driver.find_element_by_id("btnCreditCard").click()
-                    time.sleep(5)
-                    print("\nBot has Completed Checkout.\n")
-                    # Want to Receive Text Messages?
-                    client.messages.create(to="+1your number", from_="+1twilio number",
-                                           body="Bot made a Purchase. newegg !")
-            except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
-                if error:
-                    print("\nRestarting: Checkout Did Not Go Through. (Page 4)\n")
-                    time.sleep(3)
-                    yield Request(url, callback=self.parse, dont_filter=True)
-
+            # try:
+            #     print("\nBuying Product. (Page 4)\n")
+            #     time.sleep(.5)
+            #     final_step = self.driver.find_element_by_id(
+            #         "btnCreditCard").is_enabled()
+            #     if final_step:
+            #         print("\nFinalizing Bot!...\n")
+            #         # self.driver.find_element_by_id("btnCreditCard").click()
+            #         time.sleep(5)
+            #         print("\nBot has Completed Checkout.\n")
+            #         # Want to Receive Text Messages?
+            #         # client.messages.create(to="+1your number", from_="+1twilio number",
+            #         #                        body="Bot made a Purchase. newegg !")
+            # except (AttributeError, NoSuchElementException, WebDriverException, TimeoutException) as error:
+            #     if error:
+            #         print("\nRestarting: Checkout Did Not Go Through. (Page 4)\n")
+            #         time.sleep(3)
+            #         yield Request(self.product_url, callback=self.parse, dont_filter=True)
